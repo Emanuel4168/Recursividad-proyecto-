9f7d900264df3981789a1530d61e34ed5fa2d6b4
Unknown <emanuel.l4168@gmail.com>
2018-10-01 08:37:09 -0600

    Modificar Método rellenar matriz
    se modificaron todos los métodos con fines de legibilidad

--- Recursividad-proyecto/src/Recursividad/Main.java
+++ Recursividad-proyecto/src/Recursividad/Main.java
@@ -1,19 +1,45 @@
 package Recursividad;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main 
 {
 
 	public static void main(String[] args) 
 	{
-		int[][] matriz=new int[4][4];
+		Scanner scan=new Scanner(System.in);
+		//int[][] matriz=new int[2][2];
 		//System.out.println(Recursividad.sumaRenglon(matriz, 2));
 		//System.out.println(Arrays.toString(Recursividad.sumaTotalRenglones(matriz,0)));
-		//matriz=Recursividad.rellenarMatriz(0, 0, matriz);
+		//Recursividad.rellenarMatriz(matriz);
 		//for(int i=0;i<matriz.length;i++)
 			//System.out.println(Arrays.toString(matriz[i]));
-		System.out.println(Recursividad.conversorATexto("234112"));
+		//System.out.println(Recursividad.conversorATexto("102"));
+		//System.out.println(validarClave("123A"));
+		int opcion=0;
+		while(opcion!=5)
+		{
+			try
+			{
+				opcion=scan.nextInt();
+			}
+			catch(InputMismatchException err)
+			{
+				System.out.println("por favor ingrese una opción numérica");
+				opcion=scan.nextInt();
+			}
+		}
+	}
+	
+	
+	static boolean validarClave(String clave)
+	{
+		for(int i=0;i<clave.length();i++)
+			if(!Character.isDigit(clave.charAt(i)))
+				return false;
+		return true;
 	}
 
 }
--- Recursividad-proyecto/src/Recursividad/Recursividad.java
+++ Recursividad-proyecto/src/Recursividad/Recursividad.java
@@ -2,30 +2,23 @@ package Recursividad;
 
 public class Recursividad 
 {
-	private static int posicionRenglon=-1;
 	private static int[] sumasPorRenglon;
 	
-	private final static String[] UNIDADES = {"","un ", "dos ", "tres ", "cuatro ", "cinco ", "seis ", "siete ", "ocho ", "nueve "};
-    private final static String[] DECENAS = {"","diez ", "once ", "doce ", "trece ", "catorce ", "quince ", "dieciseis ",
-	        "diecisiete ", "dieciocho ", "diecinueve", "veintiuno ","veintidos ","veintitres ","veinticuatro ","veinticinco ",
-	        "veintiseis ","veintisiete ", "veintiocho ","veintinueve ","treinta ", "cuarenta ",
-	        "cincuenta ", "sesenta ", "setenta ", "ochenta ", "noventa "};
+	private final static String[] UNIDADES = {"","un ", "dos ", "tres ", "cuatro ", "cinco ", "seis ", "siete ", "ocho ", "nueve ","diez ", "once ", "doce ", "trece ",
+			"catorce ", "quince ", "dieciseis ","diecisiete ", "dieciocho ", "diecinueve", "veinte ","veintiuno ","veintidos ","veintitres ","veinticuatro ","veinticinco ",
+	        "veintiseis ","veintisiete ", "veintiocho ","veintinueve "};
+    private final static String[] DECENAS = {"","treinta ", "cuarenta ","cincuenta ", "sesenta ", "setenta ", "ochenta ", "noventa "};
 	private final static String[] CENTENAS = {"ciento ", "doscientos ", "trecientos ", "cuatrocientos ", "quinientos ", "seiscientos ",
 	        "setecientos ", "ochocientos ", "novecientos "};
 	
 	
 	
 	
-	public static int sumaRenglon(int[][] matriz,int renglon)
+	public static int sumaRenglon(int[][] matriz,int renglon,int posicionRenglon)
 	{
-		posicionRenglon+=1;
 		if(posicionRenglon==matriz[renglon].length-1)
-		{
-			int ultimoElemento=matriz[renglon][posicionRenglon];
-			posicionRenglon=-1;
-			return ultimoElemento;
-		}
-		return matriz[renglon][posicionRenglon]+sumaRenglon(matriz,renglon);
+			return matriz[renglon][posicionRenglon];
+		return matriz[renglon][posicionRenglon]+sumaRenglon(matriz,renglon,posicionRenglon+1);
 	}
 	
 	
@@ -34,7 +27,7 @@ public class Recursividad
 	{
 		if(renglon==0)
 			sumasPorRenglon=new int[matriz.length];
-		sumasPorRenglon[renglon]= sumaRenglon(matriz, renglon);
+		sumasPorRenglon[renglon]= sumaRenglon(matriz, renglon,0);
 		if(renglon==matriz.length-1)
 			return sumasPorRenglon;
 		return sumaTotalRenglones(matriz,renglon+1);
@@ -42,7 +35,46 @@ public class Recursividad
 	
 	
 	
-	public static int[][] rellenarMatriz(int fila,int columna,int[][] matriz)
+	public static void rellenarMatriz(int[][] matriz)
+	{
+		recorrerFila(0,0,matriz);
+	}
+	
+	public static String conversorATexto(String numero)
+	{
+		while(numero.length()%3!=0)
+			numero="0"+numero;
+		if(numero.length()==3)
+		{
+			if(Integer.parseInt(numero)<30)
+				return UNIDADES[Integer.parseInt(numero)];
+			if (Integer.parseInt(numero)<100)
+				return DECENAS[Integer.parseInt(numero.substring(0,1))]+" y "+UNIDADES[Integer.parseInt(numero.substring(1))];
+			if(Integer.parseInt(numero)==100)
+				return "cien";
+			if(Integer.parseInt(numero)<1000);
+			    return CENTENAS[Integer.parseInt(numero.substring(0,1))-1]+getDecenas(numero.substring(1,3));
+		}
+		if(numero.length()<7)
+			return conversorATexto(numero.substring(0,3))+" mil "+conversorATexto(numero.substring(3));
+		else if(numero.charAt(0)=='1')
+			return conversorATexto(numero.substring(0,3))+" millon "+conversorATexto(numero.substring(3));
+		if(Integer.parseInt(numero)<1000000000)
+			return conversorATexto(numero.substring(0,3))+" millones "+conversorATexto(numero.substring(3));
+		return "";
+	}
+	
+	
+	//Métodos auxiliares
+	private static String getDecenas(String numero)
+	{
+		if(numero.charAt(0)=='0')
+			return DECENAS[Integer.parseInt(numero.substring(0,1))]+UNIDADES[Integer.parseInt(numero.substring(1))];
+		return DECENAS[Integer.parseInt(numero.substring(0,1))-2]+" y "+UNIDADES[Integer.parseInt(numero.substring(1))];
+	}
+	
+	
+	private static void recorrerFila(int fila,int columna,int[][] matriz)
 	{
 		if(fila==columna)
 			matriz[fila][columna]=1;
@@ -56,44 +88,21 @@ public class Recursividad
 			matriz[fila][columna]=5;
 		else if(columna-fila>0)
 			matriz[fila][columna]=6;
-		if(fila==matriz.length-1 && columna==matriz[matriz.length-1].length-1)
-			return matriz;
-		if(columna==matriz[fila].length-1)
-			return rellenarMatriz(fila+1,0,matriz);
+		
+		if(fila==matriz.length-1)
+			recorrerColumna(fila,columna,matriz);
 		else
-			return rellenarMatriz(fila,columna+1,matriz);
+			recorrerFila(fila+1,columna,matriz);
+		if(fila!=matriz.length-1 && columna!=matriz[matriz.length-1].length-1)
+			return;
 	}
 	
-	public static String conversorATexto(String numero)
+	
+	private static void recorrerColumna(int fila,int columna,int[][] matriz)
 	{
-		while(numero.length()%3!=0)
-			numero="0"+numero;
-		if(numero.length()==3)
-		{
-			if(Integer.parseInt(numero)==10)
-				return "diez";
-			if(Integer.parseInt(numero)<10)
-				return UNIDADES[Integer.parseInt(numero)];
-			if(Integer.parseInt(numero)<100)	
-				return getDecenas(numero);
-			if(Integer.parseInt(numero)<1000);
-			    return CENTENAS[Integer.parseInt(numero.substring(0,1))-1]+getDecenas(numero.substring(1,3));
-		}
-		if(Integer.parseInt(numero)<1000000)
-			return conversorATexto(numero.substring(0,3))+" mil "+conversorATexto(numero.substring(3,6));
-		return "";
+		if(columna!=matriz[matriz.length-1].length-1)
+			recorrerFila(0,columna+1,matriz);
 	}
 	
-	
-	
-	private static String getDecenas(String numero)
-	{
-		if(Integer.parseInt(numero)==20)
-			return "veinte";
-		if(Integer.parseInt(numero)<30)
-			return DECENAS[Integer.parseInt(numero)-9];
-		if(Integer.parseInt(numero)==100)
-			return "cien";
-		return DECENAS[Integer.parseInt(numero.substring(0,1))+17]+" y "+UNIDADES[Integer.parseInt(numero.substring(1))];
-	}
+
 }
